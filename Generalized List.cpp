588323521a464a00180971b6310f60b2d64f7527
Simon <1071939594@qq.com>
2020-11-20 22:49:03 +0800

    finished part1

--- Generalized List.cpp
+++ Generalized List.cpp
@@ -1,17 +1,59 @@
 #include <iostream>
+#define ELEM char
 using namespace std;
-typedef struct GLNode
+
+struct GLNode
 {
     bool tag;
     union
     {
-        int value;
+        ELEM value;
         struct GLNode *hp;
     };
     struct GLNode *tp;
-} * GList, GLNode;
+    GLNode(int Ctag, ELEM Cvalue)
+    {
+        tag = Ctag;
+        value = Cvalue;
+    }
+    GLNode(int Ctag, GLNode *Chp)
+    {
+        tag = Ctag;
+        hp = Chp;
+    }
+    int getdepth()
+    {
+        if (tag == 0)
+        {
+            if (tp)
+                return tp->getdepth();
+            else
+                return 0;
+        }
+        if (tag == 1)
+        {
+            if (!hp)
+            {
+                if (tp)
+                    return tp->getdepth();
+                else
+                    return 0;
+            }
+            if (tp)
+                return max(1 + hp->getdepth(), tp->getdepth());
+            else
+                return 1 + hp->getdepth();
+        }
+    }
+} * GList, a(0, 'a'), b(0, 'b'), c(0, 'c'), d(0, 'd'), e(0, 'e');
+
 int main()
 {
-
+    GList = new GLNode(1, &a);
+    a.tp = new GLNode(1, &b);
+    b.tp = &c;
+    c.tp = new GLNode(1, &d);
+    d.tp = &e;
+    cout << GList->getdepth() << endl;
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define ELEM char
+using namespace std;
+int main(){
+    struct a{
+        int b;
+        a(int curr){b=curr;}
+    } c(2),d(3);
+    
+}
